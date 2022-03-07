8aa93bd692531a7ffcdb77d2cf3023a146f25ade
Mario <ma.schroed73@gmail.com>
2022-03-07 17:37:12 +0100

    remove unsupported devices

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DEVICE=enduro
+echo "Testing for $DEVICE"
 if [ -d bin ]; then
     rm -Rf bin
 fi
 mkdir bin
 
-monkeyc -d venu2plus -f monkey.jungle -o bin/pomodoro-timer.prg -y ~/develop/tools/mokey_c/developer_key --unit-test
+monkeyc -d $DEVICE -f monkey.jungle -o bin/pomodoro-timer.prg -y ~/develop/tools/mokey_c/developer_key --unit-test
 connectiq
-monkeydo bin/pomodoro-timer.prg venu2plus -t
+monkeydo bin/pomodoro-timer.prg $DEVICE -t
